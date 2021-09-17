f8de394ef89762fc271b37ff1ba395ea16156e6b
Sanjeev Banmala <banmalasanjeev@gmail.com>
2021-09-17 08:49:46 +0545

    piepline for data transformation

---
+++ Day4/src/sql/extract_employee.sql
@@ -0,0 +1,3 @@
+COPY raw_employee 
+FROM 'E:\ETL\Day4\data\employee_2021_08_01.csv' 
+WITH CSV HEADER;
---
+++ Day4/src/sql/extract_timesheet.sql
@@ -0,0 +1,3 @@
+COPY raw_timesheet 
+FROM 'E:\ETL\Day4\data\timesheet_2021_07_24.csv' 
+WITH CSV HEADER;
